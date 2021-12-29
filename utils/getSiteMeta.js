3934595cad50a5e40a578f8bd5fec65e4ff75f33
Saul Chelewani <saulchelewani@gmail.com>
2021-12-29 12:42:57 +0200

    seo optimizations

--- utils/getSiteMeta.js
+++ utils/getSiteMeta.js
@@ -1,60 +1,66 @@
-const type = "website";
-const url = "https://chelewani.co";
-const title = "Chelewani";
-const description = "We spread the solutions like [...]. Coding and application architectural solutions.";
-const mainImage = "/saul.jpg";
+const type = 'website'
+const url = 'https://chelewani.co'
+const title = 'Chelewani'
+const description = 'We spread the solutions like [...]. Coding and application architectural solutions.'
+const mainImage = '/saul.jpg'
 
 export default (meta) => {
   return [
     {
-      hid: "description",
-      name: "description",
-      content: (meta && meta.description) || description,
+      hid: 'description',
+      name: 'description',
+      content: (meta && meta.description) || description
     },
     {
-      hid: "og:type",
-      property: "og:type",
-      content: (meta && meta.type) || type,
+      hid: 'og:type',
+      property: 'og:type',
+      content: (meta && meta.type) || type
     },
     {
-      hid: "og:url",
-      property: "og:url",
-      content: (meta && meta.url) || url,
+      hid: 'og:url',
+      property: 'og:url',
+      content: (meta && meta.url) || url
     },
     {
-      hid: "og:title",
-      property: "og:title",
-      content: (meta && meta.title) || title,
+      hid: 'og:title',
+      property: 'og:title',
+      content: (meta && meta.title) || title
     },
     {
-      hid: "og:description",
-      property: "og:description",
-      content: (meta && meta.description) || description,
+      hid: 'og:description',
+      property: 'og:description',
+      content: (meta && meta.description) || description
     },
     {
-      hid: "og:image",
-      property: "og:image",
-      content: (meta && meta.mainImage) || mainImage,
+      hid: 'og:image',
+      property: 'og:image',
+      itemprop: 'image',
+      content: (meta && meta.mainImage) || mainImage
     },
     {
-      hid: "twitter:url",
-      name: "twitter:url",
-      content: (meta && meta.url) || url,
+      hid: 'twitter:card',
+      property: 'twitter:card',
+      content: 'summary_large_image'
     },
     {
-      hid: "twitter:title",
-      name: "twitter:title",
-      content: (meta && meta.title) || title,
+      hid: 'twitter:url',
+      name: 'twitter:url',
+      content: (meta && meta.url) || url
     },
     {
-      hid: "twitter:description",
-      name: "twitter:description",
-      content: (meta && meta.description) || description,
+      hid: 'twitter:title',
+      name: 'twitter:title',
+      content: (meta && meta.title) || title
     },
     {
-      hid: "twitter:image",
-      name: "twitter:image",
-      content: (meta && meta.mainImage) || mainImage,
+      hid: 'twitter:description',
+      name: 'twitter:description',
+      content: (meta && meta.description) || description
     },
-  ];
+    {
+      hid: 'twitter:image',
+      name: 'twitter:image',
+      content: (meta && meta.mainImage) || mainImage
+    }
+  ]
 };
