b8b74df1427b4c30c2d6a48167cbd58fe99bd278
Saul Chelewani <saulchelewani@gmail.com>
2021-12-30 11:50:24 +0200

    error page

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,7 +1,7 @@
 <template>
   <Banner large>
     <h1 class="text-rose-500 text-3xl md:text-6xl font-mono text-center">
-      <span>[<span class="text-cyan-500">...404</span>]</span>
+      <span>[<span class="text-cyan-500">...{{ error.statusCode }}</span>]</span>
     </h1>
     <h2 class='text-cyan-400 font-mono text-center pt-8 hover:underline'><nuxt-link to='/'>Go to home page</nuxt-link></h2>
   </Banner>
